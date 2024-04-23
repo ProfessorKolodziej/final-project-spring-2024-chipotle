26f00059b3c06cacc9c99fe230f5b231c53b5e95
marytshe <marytshe@bu.edu>
2024-04-23 19:37:48 -0400

    hhh

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -129,10 +129,6 @@ addClickToggleBackground("choice_guac");
 addClickToggleBackground("choice_queso");
 addClickToggleBackground("choice_salsa");
 addClickToggleBackground("choice_green_salsa");
-// This is where you should write all JavaScript
-// for your project. Remember a few things as you start!
-// - Use let or const for all variables
-// - Do not use jQuery - use JavaScript instead
-// - Do not use onclick - use addEventListener instead
-// - Run npm run test regularly to check autograding
-// - You'll need to link this file to your HTML :)
+
+
+
