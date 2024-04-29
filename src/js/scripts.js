c3be204fd9611a42739983928a795891205718da
IndiaStevenson <157330310+IndiaStevenson@users.noreply.github.com>
2024-04-28 21:30:02 -0400

    Update scripts.js after accident

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Create the button element
     let startButton = document.createElement("button");
     startButton.textContent = "Start"; // Set the text content of the button
-    startButton.className = "continue";
+
     // Add event listener to navigate to the next page when clicked
     startButton.addEventListener("click", function() {
         window.location.href = "pages/how_to_play.html";
