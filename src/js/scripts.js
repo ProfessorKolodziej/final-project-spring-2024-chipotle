39b1cadfe5f097ab51b6184ccefb472865115839
marytshe <marytshe@bu.edu>
2024-04-28 22:17:39 -0400

    Revert "jj"
    This reverts commit 9989650a757551d7270d23e917f0c5177a2c2ae8.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,13 +1,134 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Create the button element
-    let startButton = document.createElement("button");
-    startButton.textContent = "Start"; // Set the text content of the button
+const SELECTED_COLOR = "grey";
+const UNSELECTED_COLOR = "white";
 
-    // Add event listener to navigate to the next page when clicked
-    startButton.addEventListener("click", function() {
-        window.location.href = "pages/how_to_play.html";
-    });
-    // Append the button to the main element with class "content-container"
-    let contentContainer = document.querySelector(".content-container");
-    contentContainer.appendChild(startButton);
+document.getElementById("choice_bowl").addEventListener("click", function() {
+    if (document.getElementById("choice_bowl").style.background == SELECTED_COLOR) {
+        document.getElementById("choice_bowl").style.background=UNSELECTED_COLOR;
+    } else {
+        document.getElementById("choice_bowl").style.background=SELECTED_COLOR;
+    }
+    document.getElementById("choice_burrito").style.background=UNSELECTED_COLOR;
+    document.getElementById("choice_lifestyle_bowl").style.background=UNSELECTED_COLOR;
 });
+
+document.getElementById("choice_burrito").addEventListener("click", function() {
+    if (document.getElementById("choice_burrito").style.background == SELECTED_COLOR) {
+        document.getElementById("choice_burrito").style.background=UNSELECTED_COLOR;
+    } else {
+        document.getElementById("choice_burrito").style.background=SELECTED_COLOR;
+    }
+    document.getElementById("choice_bowl").style.background=UNSELECTED_COLOR;
+    document.getElementById("choice_lifestyle_bowl").style.background=UNSELECTED_COLOR;
+});
+
+document.getElementById("choice_lifestyle_bowl").addEventListener("click", function() {
+    if (document.getElementById("choice_lifestyle_bowl").style.background == SELECTED_COLOR) {
+        document.getElementById("choice_lifestyle_bowl").style.background=UNSELECTED_COLOR;
+    } else {
+        document.getElementById("choice_lifestyle_bowl").style.background=SELECTED_COLOR;
+    }
+    document.getElementById("choice_burrito").style.background=UNSELECTED_COLOR;
+    document.getElementById("choice_bowl").style.background=UNSELECTED_COLOR;
+});
+
+
+
+
+const SELECTED_COLOR = "grey";
+const UNSELECTED_COLOR = "white";
+
+
+document.getElementById("choice_white_rice").addEventListener("click", function() {
+    if (document.getElementById("choice_white_rice").style.background == SELECTED_COLOR) {
+        document.getElementById("choice_white_rice").style.background=UNSELECTED_COLOR;
+    } else {
+        document.getElementById("choice_white_rice").style.background=SELECTED_COLOR;
+    }
+    document.getElementById("choice_brown_rice").style.background=UNSELECTED_COLOR;
+});
+
+document.getElementById("choice_brown_rice").addEventListener("click", function() {
+    if (document.getElementById("choice_brown_rice").style.background == SELECTED_COLOR) {
+        document.getElementById("choice_brown_rice").style.background=UNSELECTED_COLOR;
+    } else {
+        document.getElementById("choice_brown_rice").style.background=SELECTED_COLOR;
+    }
+    document.getElementById("choice_white_rice").style.background=UNSELECTED_COLOR;
+});
+
+
+
+const SELECTED_COLOR = "grey";
+const UNSELECTED_COLOR = "white";
+
+
+
+
+document.getElementById("choice_black_beans").addEventListener("click", function() {
+    if (document.getElementById("choice_black_beans").style.background == SELECTED_COLOR) {
+        document.getElementById("choice_black_beans").style.background=UNSELECTED_COLOR;
+    } else {
+        document.getElementById("choice_black_beans").style.background=SELECTED_COLOR;
+    }
+    document.getElementById("choice_pinto_beans").style.background=UNSELECTED_COLOR;
+});
+
+document.getElementById("choice_pinto_beans").addEventListener("click", function() {
+    if (document.getElementById("choice_pinto_beans").style.background == SELECTED_COLOR) {
+        document.getElementById("choice_pinto_beans").style.background=UNSELECTED_COLOR;
+    } else {
+        document.getElementById("choice_pinto_beans").style.background=SELECTED_COLOR;
+    }
+    document.getElementById("choice_black_beans").style.background=UNSELECTED_COLOR;
+});
+
+
+
+const SELECTED_COLOR = "grey";
+const UNSELECTED_COLOR = "white";
+
+
+function addClickToggleBackground(elementId) {
+    document.getElementById(elementId).addEventListener("click", function() {
+        if (document.getElementById(elementId).style.background == SELECTED_COLOR) {
+            document.getElementById(elementId).style.background=UNSELECTED_COLOR;
+        } else {
+            document.getElementById(elementId).style.background=SELECTED_COLOR;
+        }
+    });
+}
+
+addClickToggleBackground("choice_steak");
+addClickToggleBackground("choice_beef");
+addClickToggleBackground("choice_chicken");
+addClickToggleBackground("choice_veggies");
+
+
+
+const SELECTED_COLOR = "grey";
+const UNSELECTED_COLOR = "white";
+
+
+
+function addClickToggleBackground(elementId) {
+    document.getElementById(elementId).addEventListener("click", function() {
+        if (document.getElementById(elementId).style.background == SELECTED_COLOR) {
+            document.getElementById(elementId).style.background=UNSELECTED_COLOR;
+        } else {
+            document.getElementById(elementId).style.background=SELECTED_COLOR;
+        }
+    });
+}
+
+addClickToggleBackground("choice_lettuce");
+addClickToggleBackground("choice_sour_cream");
+addClickToggleBackground("choice_cheese");
+addClickToggleBackground("choice_veggies");
+addClickToggleBackground("choice_corn");
+addClickToggleBackground("choice_guac");
+addClickToggleBackground("choice_queso");
+addClickToggleBackground("choice_salsa");
+addClickToggleBackground("choice_green_salsa");
+
+
+
