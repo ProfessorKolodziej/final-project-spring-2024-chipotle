de5cb770e52b704b8829d3a8d20e1004e54acd88
marytshe <marytshe@bu.edu>
2024-04-30 09:36:23 -0400

    play again button

--- src/js/byex.js
+++ src/js/byex.js
@@ -1,12 +1,8 @@
-console.log
 document.addEventListener("DOMContentLoaded", function() {
-    // Create the button element
-    let startButton = document.createElement("button");
-    startButton.textContent = "Start"; // Set the text content of the button
-    startButton.className = "continue";
-    // Add event listener to navigate to the next page when clicked
-    startButton.addEventListener("click", function() {
+    // Find the "Play Again" button element
+    let playAgainButton = document.querySelector(".continue button");
+    // Add event listener to navigate to the step_1.html when clicked
+    playAgainButton.addEventListener("click", function() {
         window.location.href = "/pages/step_1.html";
     });
-
 });
