1819f7cdf52341e04e5f3d4b3adc6b8f19373534
marytshe <marytshe@bu.edu>
2024-04-29 20:35:21 -0400

    new6

--- src/js/scripts1.js
+++ src/js/scripts1.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Create the button element
     let startButton = document.createElement("button");
-    startButton.textContent = "Start"; // Set the text content of the button
+    startButton.textContent = "Continue"; // Set the text content of the button
 
     // Add event listener to navigate to the next page when clicked
     startButton.addEventListener("click", function() {
-        window.location.href = "/pages/how_to_play.html";
+        window.location.href = "/pages/step_1.html";
     });
     // Append the button to the main element with class "content_container"
     let contentContainer = document.querySelector(".content_container");
